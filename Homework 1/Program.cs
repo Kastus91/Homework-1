2557859e207996743304f52cd5f673e3c2082a0f
Костян <@DESKTOP-U6J4DL1>
2022-02-28 17:35:09 +0300

    Add project files.

---
+++ Homework 1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача №1 Дискрименант и его корни
+
+            /*Console.WriteLine($"Введите коэффициенты квадратного уравнения ax^2 + bx + c = 0 ");
+
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("c = ");
+            int c = int.Parse(Console.ReadLine());
+
+            double d = b * b - 4 * a * c;
+            Console.WriteLine($"Дискриминант = {d}");
+
+
+            double x1 = (-b - Math.Sqrt(d)) / 2 * a;
+            double x2 = (-b - Math.Sqrt(d)) / 2 * a;
+
+            Console.WriteLine($"Корни уравнения x1 = {x1}, x2 = {x2}");*/
+
+
+            
+            // Задача №2 Конвертёр валют
+
+            /*double usdToByn = 3.2;
+
+            Console.WriteLine("Введите сумму в долларах: ");
+
+            double usd = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Вам нужно: " + usd * usdToByn + "белорусских рублей");*/
+
+            
+            
+            // Задача №3 рассчитать площадь прямоугольника
+
+            /*Console.WriteLine("Введите размер стороны а: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите размер стороны b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Площадь квадрата состовляет: " + a * b);*/
+
+
+
+
+        }
+    }
+}
